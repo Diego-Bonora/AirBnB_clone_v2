6033994cc649a2819085e2157b3981b45c48c640
Diego-Bonora <bonoradi@gmail.com>
2023-08-15 06:58:05 -0700

    default input for the text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" first flask web app """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ First app rout for the proyect """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Second app rout for the proyect """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ third app rout for the proyect """
+    text_no_underscore = text.replace("_", " ")
+    return "C {}".format(text_no_underscore)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is_cool"):
+    """ fourth app rout for the proyect """
+    text_no_underscore = text.replace("_", " ")
+    return "C {}".format(text_no_underscore)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
